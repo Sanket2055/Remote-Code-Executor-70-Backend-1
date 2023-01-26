287a1072fd09c17184251be4f91a06568c01c62e
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2023-01-26 14:57:30 +0530

    Added C and Go

--- routes/exeRoutes.js
+++ routes/exeRoutes.js
@@ -7,4 +7,8 @@ router.post("/exe-js", exeController.executeJS);
 
 router.post("/exe-py", exeController.executePY);
 
+router.post("/exe-c", exeController.executeC);
+
+router.post("/exe-go", exeController.executeGO);
+
 module.exports = router;
